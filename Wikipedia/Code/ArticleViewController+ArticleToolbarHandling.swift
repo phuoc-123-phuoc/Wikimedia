cf1ff6793d3c3232d532a9ab63c4312445596148
Lê Vũ Phước <82875240+phuoc-123-phuoc@users.noreply.github.com>
2023-04-28 23:22:46 +0700

    Wikimedia

---
+++ Wikipedia/Code/ArticleViewController+ArticleToolbarHandling.swift
@@ -0,0 +1,45 @@
+extension ArticleViewController: ArticleToolbarHandling {
+    func showTableOfContents(from controller: ArticleToolbarController) {
+        showTableOfContents()
+    }
+    
+    func hideTableOfContents(from controller: ArticleToolbarController) {
+        hideTableOfContents()
+    }
+    
+    var isTableOfContentsVisible: Bool {
+        return tableOfContentsController.viewController.displayMode == .inline && tableOfContentsController.viewController.isVisible
+    }
+    
+    func toggleSave(from controller: ArticleToolbarController) {
+        let isSaved = dataStore.savedPageList.toggleSavedPage(for: articleURL)
+        if isSaved {
+            readingListsFunnel.logArticleSaveInCurrentArticle(articleURL)
+        } else {
+            readingListsFunnel.logArticleUnsaveInCurrentArticle(articleURL)
+        }
+    }
+    
+    func showThemePopover(from controller: ArticleToolbarController) {
+        themesPresenter.showReadingThemesControlsPopup(on: self, responder: self, theme: theme)
+    }
+    
+    func saveButtonWasLongPressed(from controller: ArticleToolbarController) {
+        let addArticlesToReadingListVC = AddArticlesToReadingListViewController(with: dataStore, articles: [article], theme: theme)
+        let nc = WMFThemeableNavigationController(rootViewController: addArticlesToReadingListVC, theme: theme)
+        nc.setNavigationBarHidden(false, animated: false)
+        present(nc, animated: true)
+    }
+    
+    func showLanguagePicker(from controller: ArticleToolbarController) {
+        showLanguages()
+    }
+    
+    func share(from controller: ArticleToolbarController) {
+        shareArticle()
+    }
+    
+    func showFindInPage(from controller: ArticleToolbarController) {
+        showFindInPage()
+    }
+}
